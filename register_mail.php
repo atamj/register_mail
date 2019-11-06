568d8fbf7cc7062b0c2200ec807f0c80b7fcaa8e
root <root@localhost.localdomain>
2019-11-06 13:27:12 -0400

    first commit

---
+++ register_mail.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Plugin Name:        Register Mail
+Plugin URI:         register_mail
+Description:        Plugin qui permet d'envoyer un mail à l'admin à l'inscription d'un nouvel utilisateur
+Version:            1.0.0
+Author:             Jaël Atam
+Author URI:         portfolio.jaelatam.com
+*/
+
+class register_mail
+{
+    public function __construct()
+    {
+        add_action('user_register',[$this,'register_mail']);
+    }
+
+    public function register_mail(){
+        if (isset($_POST["email"])){
+            $user_mail = $_POST["email"];
+            $site_name = get_bloginfo('name');
+            $to = get_option('admin_email');
+            $subject = $_POST["register"];
+            $message = "<p style='font-size: 18px'>Bonjour,</p>
+                    <p style='font-size: 18px'>".$subject." d'un nouvel utilisateur sous l'adresse courriel : '".$user_mail."'.<br>
+                    Vous pouvez maintenant le contacter pour le rajouter dans un groupe.</p>
+                    <p style='font-size: 18px'>Cordialement,</p>";
+            $headers = array('Content-Type: text/html; charset=UTF-8', "From: ".$site_name." < ne_pas_repondre@".str_ireplace("http://", "", get_bloginfo('url'))." >");
+            wp_mail($to, $subject, $message, $headers);
+        }
+
+    }
+}
+
+new register_mail();
